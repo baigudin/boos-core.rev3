e37f332fbd15ef87724ca55eaa2be69b10b3c180
Sergey Baigudin <sergey@baigudin.software>
2017-09-01 17:34:53 +0300

    TI TMS320C645x INTC is being developed

--- source/driver/ti/tms320c64x/driver.InterruptController.hpp
+++ source/driver/ti/tms320c64x/driver.InterruptController.hpp
@@ -31,26 +31,26 @@ namespace driver
      */
     enum Source 
     {
-      DSPINT     = 0x00, //Host port host to DSP interrupt
-      TINT0      = 0x01, //Timer 0 interrupt
-      TINT1      = 0x02, //Timer 1 interrupt
-      SD_INTA    = 0x03, //EMIFA SDRAM timer interrupt
-      GPINT4     = 0x04, //GPIO interrupt 4/External interrupt 4
-      GPINT5     = 0x05, //GPIO interrupt 5/External interrupt 5
-      GPINT6     = 0x06, //GPIO interrupt 6/External interrupt 6
-      GPINT7     = 0x07, //GPIO interrupt 7/External interrupt 7
-      EDMA_INT   = 0x08, //EDMA channel (0 through 63) interrupt
-      XINT0      = 0x0c, //McBSP 0 transmit interrupt
-      RINT0      = 0x0d, //McBSP 0 receive interrupt
-      XINT1      = 0x0e, //McBSP 1 transmit interrupt
-      RINT1      = 0x0f, //McBSP 1 receive interrupt
-      GPINT0     = 0x10, //GPIO interrupt 0
-      XINT2      = 0x11, //McBSP 2 transmit interrupt
-      RINT2      = 0x12, //McBSP 2 receive interrupt
-      TINT2      = 0x13, //Timer 2 interrupt
-      SD_INTB    = 0x14, //EMIFB SDRAM timer interrupt
-      PCI_WAKEUP = 0x15, //PCI wakeup interrupt
-      UINT       = 0x17  //UTOPIA interupt
+      DSPINT     = 0x00, // Host port host to DSP interrupt
+      TINT0      = 0x01, // Timer 0 interrupt
+      TINT1      = 0x02, // Timer 1 interrupt
+      SD_INTA    = 0x03, // EMIFA SDRAM timer interrupt
+      GPINT4     = 0x04, // GPIO interrupt 4/External interrupt 4
+      GPINT5     = 0x05, // GPIO interrupt 5/External interrupt 5
+      GPINT6     = 0x06, // GPIO interrupt 6/External interrupt 6
+      GPINT7     = 0x07, // GPIO interrupt 7/External interrupt 7
+      EDMA_INT   = 0x08, // EDMA channel (0 through 63) interrupt
+      XINT0      = 0x0c, // McBSP 0 transmit interrupt
+      RINT0      = 0x0d, // McBSP 0 receive interrupt
+      XINT1      = 0x0e, // McBSP 1 transmit interrupt
+      RINT1      = 0x0f, // McBSP 1 receive interrupt
+      GPINT0     = 0x10, // GPIO interrupt 0
+      XINT2      = 0x11, // McBSP 2 transmit interrupt
+      RINT2      = 0x12, // McBSP 2 receive interrupt
+      TINT2      = 0x13, // Timer 2 interrupt
+      SD_INTB    = 0x14, // EMIFB SDRAM timer interrupt
+      PCI_WAKEUP = 0x15, // PCI wakeup interrupt
+      UINT       = 0x17  // UTOPIA interupt
     };  
   
     /** 
